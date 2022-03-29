05cbfafeadd47d523b0db852e67b18f6b28ace3f
tamngoduc <ductam9977@gmail.com>
2022-03-29 13:25:37 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+/*
+* Đầu vào:
+- Số nguyên n
+* Xử lí:
+- Tạo hàm kiểm tra số nguyên tố
+- Khai báo biến số nguyên
+- Nếu số nguyên nhỏ hơn 2 thì yêu cầu nhập lại
+- Khơi tạo biến kết quả
+- Sử dụng vòng lặp for với điều kiện if
+- Gọi hàm kiểm tra số nguyên tố, nếu giá trị bằng true thì cộng dồn vào biến kết quả
+* Đầu ra:
+- In ra màn hình các số nguyên tố từ 1 đến n
+*/
+
 function checkPrimeNumber(n) {
   if (n < 2) {
     return false;
@@ -12,11 +26,15 @@ function checkPrimeNumber(n) {
 
 document.getElementById("btnPrimeNumber").addEventListener("click", function () {
   var n = document.getElementById("txtNumber").value;
-  var result = "";
-  for (var i = 0; i < n; i++) {
-    if (checkPrimeNumber(i) == true) {
-      result += i + " ";
+  if (n < 2) {
+    alert("Bạn hãy nhập số lớn hơn 2");
+  } else {
+    var result = "";
+    for (var i = 0; i < n; i++) {
+      if (checkPrimeNumber(i) == true) {
+        result += i + " ";
+      }
     }
+    document.getElementById("primeNumber").innerHTML = result;
   }
-  document.getElementById("primeNumber").innerHTML = result;
 });
